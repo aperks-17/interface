ddd93a9e557bb4c19a9eb980cbeb510b05b05971
aperks-17 <46737512+aperks-17@users.noreply.github.com>
2019-04-27 21:48:14 -0600

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,10 @@
+package application;
+
+import java.util.Date;
+
+public class Assignment {
+		public String Name;
+		public String ClassName;
+		public String AssignedDate;
+		public String DueDate;
+}
